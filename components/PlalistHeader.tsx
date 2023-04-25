5205df12e3342f50cee34350db06c3e5b2daff7b
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2023-04-25 13:59:53 -0300

    add-photo-playList

--- components/PlalistHeader.tsx
+++ components/PlalistHeader.tsx
@@ -13,36 +13,36 @@ export function PlayListHeader() {
             </button>
           </a>
           <a href='#' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-            <Image src="/imagesalbum01.png" width={104} height={104} alt="PlayList"/>
-            <strong>Wasting Light</strong>
+            <Image src="/imagem02.png" width={104} height={104} alt="PlayList"/>
+            <strong>Chandle Moore</strong>
             <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
               <Play />
             </button>
           </a>
           <a href='#' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-            <Image src="/imagesalbum01.png" width={104} height={104} alt="PlayList"/>
-            <strong>Wasting Light</strong>
+            <Image src="/imagem04.png" width={104} height={104} alt="PlayList"/>
+            <strong>Anos 80</strong>
             <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
               <Play />
             </button>
           </a>
           <a href='#' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-            <Image src="/imagesalbum01.png" width={104} height={104} alt="PlayList"/>
-            <strong>Wasting Light</strong>
+            <Image src="/imagem05.png" width={104} height={104} alt="PlayList"/>
+            <strong>Ton Molinari</strong>
             <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
               <Play />
             </button>
           </a>
           <a href='#' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-            <Image src="/imagesalbum01.png" width={104} height={104} alt="PlayList"/>
-            <strong>Wasting Light</strong>
+            <Image src="/imagem06.png" width={104} height={104} alt="PlayList"/>
+            <strong>50 Cent</strong>
             <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
               <Play />
             </button>
           </a>
           <a href='#' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-            <Image src="/imagesalbum01.png" width={104} height={104} alt="PlayList"/>
-            <strong>Wasting Light</strong>
+            <Image src="/imagem07.png" width={104} height={104} alt="PlayList"/>
+            <strong>Eminem</strong>
             <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
               <Play />
             </button>
--- components/PlayListBack.tsx
+++ components/PlayListBack.tsx
@@ -1,37 +1,83 @@
 import Image from 'next/image';
+import { Play } from 'lucide-react'
 
 export function PlayListBack() {
   return (
     <div className='grid grid-cols-8 gap-4 mt-4'>
 
-          <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bgwhite/10'>
-            <Image src="/imagesalbum01.png" className='w-full' width={104} height={104} alt="PlayList"/>
-            <strong className='font-semibold'>Play List</strong>
-            <span className='text-sm text-zinc-500'>Wallows, COIN, girl in red and more</span>
+          <a href='#' className='bg-white/5 group p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors'>
+            <Image src="/imagem10.png" className='w-full' width={104} height={104} alt="PlayList"/>
+            <strong className='font-semibold'>Onze20</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <a href='https://www.youtube.com/results?search_query=onze20' target="_blank">
+                <Play />
+              </a>
+            </button>
+            <span className='text-sm text-zinc-500'>
+              Gênero: Reggae
+              atividade desde: 2006
+              Gravadoras:Maynard
+            </span>
           </a>
 
-          <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bgwhite/10'>
-            <Image src="/imagesalbum01.png" className='w-full' width={104} height={104} alt="PlayList"/>
-            <strong className='font-semibold'>Play List</strong>
-            <span className='text-sm text-zinc-500'>Wallows, COIN, girl in red and more</span>
+          <a href='#' className='bg-white/5 p-3 group rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors'>
+            <Image src="/imagem11.png" className='w-full' width={104} height={104} alt="PlayList"/>
+            <strong className='font-semibold'>3030</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <a href='https://www.youtube.com/results?search_query=3030' target="_blank">
+                <Play />
+              </a>
+            </button>
+            <span className='text-sm text-zinc-500'>
+              Gênero: Hip-Hop/rap
+              atividade desde: 2010
+              Gravadoras: Austro Music, 30/30 ...
+            </span>
           </a>
 
-          <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bgwhite/10'>
-            <Image src="/imagesalbum01.png" className='w-full' width={104} height={104} alt="PlayList"/>
-            <strong className='font-semibold'>Play List</strong>
-            <span className='text-sm text-zinc-500'>Wallows, COIN, girl in red and more</span>
+          <a href='#' className='bg-white/5 group p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors'>
+            <Image src="/imagem12.png" className='w-full' width={104} height={104} alt="PlayList"/>
+            <strong className='font-semibold'>Poesia Acustica11</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <a href='https://www.youtube.com/results?search_query=poesia+11' target="_blank">
+                <Play />
+              </a>
+            </button>
+            <span className='text-sm text-zinc-500'>
+              criador foi o carioca Paulo Alvarez, 
+              Aos 25 anos, em 2015. 
+              Comecei a cursar Direito.
+            </span>
           </a>
 
-          <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bgwhite/10'>
-            <Image src="/imagesalbum01.png" className='w-full' width={104} height={104} alt="PlayList"/>
-            <strong className='font-semibold'>Play List</strong>
-            <span className='text-sm text-zinc-500'>Wallows, COIN, girl in red and more</span>
+          <a href='#' className='bg-white/5 p-3 group rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors'>
+            <Image src="/imagem08.png" className='w-full' width={104} height={104} alt="PlayList"/>
+            <strong className='font-semibold'>Bruno Mars</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <a href='https://www.youtube.com/results?search_query=bruno+mars' target="_blank">
+                <Play />
+              </a>
+            </button>
+            <span className='text-sm text-zinc-500'>
+              Pais: Bernadette, Peter 
+              Irmãos: Presley Hernandez
+              Gênero: Pop
+            </span>
           </a>
 
-          <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bgwhite/10'>
-            <Image src="/imagesalbum01.png" className='w-full' width={104} height={104} alt="PlayList"/>
-            <strong className='font-semibold'>Play List</strong>
-            <span className='text-sm text-zinc-500'>Wallows, COIN, girl in red and more</span>
+          <a href='#' className='bg-white/5 p-3 group rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors'>
+            <Image src="/imagem09.png" className='w-full' width={104} height={104} alt="PlayList"/>
+            <strong className='font-semibold'>John Legend</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <a href='https://www.youtube.com/results?search_query=john+legend' target="_blank">
+                <Play />
+              </a>
+            </button>
+            <span className='text-sm text-zinc-500'>
+              Cônjuge: Chrissy Teigen (desde 2013)
+              Pais: Ronald Stephens, Phyllis Stephens
+              Irmãos: Ronald Stephens II,
+            </span>
           </a>
 
         </div>
